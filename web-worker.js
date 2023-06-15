7a34f8bd822168549302ea039a63f9da1e2e5424
B. Ricks <bricks@unomaha.edu>
2023-06-15 13:35:33 -0500

    Fix off by one bug

--- web-worker.js
+++ web-worker.js
@@ -31,16 +31,23 @@ function randomName() {
 
 function getRandomUsernames(size, usernames = []) {
   //let usernames = []
-  while (usernames.length < size) {
-    let r1 = randomName();
-    let r2 = randomName();
-    let email = r1 + r2 + "@unomaha.edu"
-    // if (!usernames.includes(email)) {
-    usernames.push(email)
-    // }
+  try {
+    while (usernames.length < size && usernames.length < 40_000_000) {
+      let r1 = randomName();
+      let r2 = randomName();
+      if (r1 == r2) continue;
+      let email = r1 + r2 + "@unomaha.edu"
+      // if (!usernames.includes(email)) {
+      usernames.push(email)
+      // }
 
+    }
+  } catch (e) {
+    console.error(e)
   }
-  return usernames
+  finally {
+    return usernames
+  }
 }
 
 function findInArray(array, value) {
@@ -52,36 +59,35 @@ function findInArray(array, value) {
 }
 
 
-let size = 2048;
-let maxSize = 10_000_000;
+let size = 2 ** 23;
+let maxSize = 2 ** 24;
 let usernames = getRandomUsernames(size)
 
 
-while (size < maxSize) {
+while (size <= maxSize) {
 
-
-
-  let toFind = usernames[Math.floor(usernames.length / 4)];
-  let unfindable = "aa@unomaha.edu"
-
-  let timeUnfindable = timeIt(() => findInArray(usernames, unfindable)).time
-  let timeToFind = timeIt(() => findInArray(usernames, toFind)).time
-
-  console.log(`When the size is ${size.f()}/${maxSize.f()}, then it took the following times to find usernames: ${timeToFind}, ${timeUnfindable}`)
-
-  size *= 2
   try {
-    console.log("Starting to make a new Array")
     let time;
     let result
-    ({ time, result } = timeIt(() => getRandomUsernames(size * 2, usernames)));
+    ({ time, result } = timeIt(() => getRandomUsernames(size, usernames)));
     usernames = result;
-    console.log("Ending making a new Array with a speed of " + time.floor().f())
+    console.log("Made a new array of size " + usernames.length.f() + " with a speed of " + time.floor().f())
   } catch (e) {
     console.error(e)
     console.log("It doesn't look like we can get an array that's any bigger.")
     break;
   }
+
+  let toFind = usernames[Math.floor(usernames.length / 4)];
+  let unfindable = "EmmaEmma@unomaha.edu"//usernames[Math.floor(usernames.length / 2)]
+
+  let timeUnfindable = timeIt(() => findInArray(usernames, unfindable)).time
+  let timeToFind = timeIt(() => findInArray(usernames, toFind)).time
+
+  console.log(`When the size is ${usernames.length.f()}/${maxSize.f()}, then it took the following times to find usernames: ${timeToFind}, ${timeUnfindable}`)
+
+  size *= 2
+
 
   //console.log(usernames)
   //break;
